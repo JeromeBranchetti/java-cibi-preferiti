36519d1251b7a3c19ce895ddae7f988f838cab7f
JEROMEBRANCHETTI <jeromebranchetti00@gmail.com>
2021-11-17 14:48:17 +0100

    source

---
+++ src/org/generation/italy/array/CibiPreferiti.java
@@ -0,0 +1,18 @@
+package org.generation.italy.array;
+
+public class CibiPreferiti {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] listaCibi = {"pasta", "riso", "carote", "pizza", "patatine", "kebabm", "insalata", "zucchine", "ciccia", "cavolfiore" };
+		int numeroCibi = listaCibi.length;
+		System.out.println("La mia lista contiene " + numeroCibi + " cibi.");
+		System.out.println("Il mio cibo preferito è " + listaCibi[0]);
+		System.out.println("Quello che mi piace meno invece è " + listaCibi[numeroCibi -1]);
+		
+		
+		
+		
+	}
+
+}
